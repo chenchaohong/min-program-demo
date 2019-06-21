26e6cb24195b64b64d9f181e13d6ea3cb7dafcf1
kanade <923928480@qq.com>
2019-06-21 17:11:48 +0800

    Signed-off-by: kanade <923928480@qq.com>

---
+++ src/pages/scan/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="scan">扫码</button>
+        <span>扫的码是：{{scanName}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            scanName: ''
+        }
+    },
+    created () {
+    },
+    methods: {
+        scan () {
+            wx.scanCode({
+                success: (res) => {
+                    this.scanName = res.result
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
